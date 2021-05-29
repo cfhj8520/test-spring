76e6ad44a2add052d69b300e866a0e3d1048938a
DESKTOP-P8G6A4G\Hyunjun <cfhj8520@naver.com>
2021-05-29 14:59:04 +0900

    member.java 자동 벨류 관리

--- src/main/java/test/testspring/domain/Member.java
+++ src/main/java/test/testspring/domain/Member.java
@@ -1,7 +1,12 @@
 package test.testspring.domain;
 
+import javax.persistence.*;
+
+@Entity
 public class Member {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
